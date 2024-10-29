4c86e400ef5ea2f7c4319e1096961d49a5d9713b
Ekaterina Briuchanova <ueobrn@yandex.ru>
2024-10-29 18:02:36 +0700

    add lombok

--- src/main/java/model/Credentials.java
+++ src/main/java/model/Credentials.java
@@ -1,30 +1,20 @@
 package model;
 
-public class Credentials {
-    private String email;
-    private String password;
+import lombok.Getter;
+import lombok.Setter;
 
+public class Credentials {
+
+@Getter
+@Setter
+private String email;
+    @Getter @Setter
+    private String password;
     public Credentials(String email, String password) {
         this.email = email;
         this.password = password;
     }
     public Credentials() {
-    }
-
-public String getEmail() {
-    return email;
-}
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,8 +1,15 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class User {
+    @Getter
+    @Setter
     private String email;
+    @Getter @Setter
     private String password;
+    @Getter @Setter
     private String name;
 
     //конструктор с параметром
@@ -16,29 +23,5 @@ public class User {
     public User() {
     }
 
-    //сеттеры и геттеры
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
